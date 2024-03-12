2d23ef72ccfe732a5e6802d8241dfd47747da168
MilosJovic23 <milos.mj992@gmail.com>
2024-03-12 16:56:00 +0100

    * initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//
+
+function promenaBoje() {
+	document.getElementById("promena").style.color = "royalblue";
+}
+document.getElementById("promena").addEventListener("click", promenaBoje);
+
+//
+function promenaFonta() {
+	document.getElementById("fontSize").style.fontSize = "6rem";
+}
+document.getElementById("fontSize").addEventListener("click", promenaFonta);
