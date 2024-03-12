7b3190ded1adbb89f92bc3d2e393f855089a9cb6
MilosJovic23 <milos.mj992@gmail.com>
2024-03-12 16:56:24 +0100

    * small changes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //
 
+
 function promenaBoje() {
 	document.getElementById("promena").style.color = "royalblue";
 }
 document.getElementById("promena").addEventListener("click", promenaBoje);
 
 //
+
+
 function promenaFonta() {
 	document.getElementById("fontSize").style.fontSize = "6rem";
 }
 document.getElementById("fontSize").addEventListener("click", promenaFonta);
+
