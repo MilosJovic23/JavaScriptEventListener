058fbc63988d99581f9d98702d6e3fcccd4360d5
MilosJovic23 <milos.mj992@gmail.com>
2024-12-26 11:48:46 +0100

    * few changes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //
 
 
-function promenaBoje() {
-	document.getElementById("promena").style.color = "royalblue";
-}
-document.getElementById("promena").addEventListener("click", promenaBoje);
+	function ColorChange() {
+		document.getElementById("colorChange").style.color = "royalblue";
+	}
+	document.getElementById("colorChange").addEventListener("click", ColorChange);
 
 //
 
 
-function promenaFonta() {
-	document.getElementById("fontSize").style.fontSize = "6rem";
-}
-document.getElementById("fontSize").addEventListener("click", promenaFonta);
+	function FontChange() {
+		document.getElementById("fontSize").style.fontSize = "6rem";
+	}
+	document.getElementById("fontSize").addEventListener("click", FontChange);
 
